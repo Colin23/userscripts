ed7a876ab737e77015978fe8c15a1141acf3e83d
Colin23 <44360448+Colin23@users.noreply.github.com>
2024-11-17 15:58:32 +0100

    feat: Add a repo scaffold to ease development and ensure consistency

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import js from "@eslint/js";
+
+/** @type {import("eslint").Linter.Config[]} */
+export default [
+    js.configs.recommended,
+    {
+        files: ["**/*.js"],
+        languageOptions: { sourceType: "script" }
+    },
+    {
+        languageOptions: { globals: globals.browser }
+    },
+    pluginJs.configs.recommended
+];
