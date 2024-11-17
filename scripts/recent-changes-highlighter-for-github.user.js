3362a2052ec517125e090317880952ae7ad492d5
Colin23 <44360448+Colin23@users.noreply.github.com>
2024-11-17 16:41:40 +0100

    feat: Add more colors depending on the most recent commit age

--- scripts/recent-changes-highlighter-for-github.user.js
+++ scripts/recent-changes-highlighter-for-github.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Recent Changes Highlighter for GitHub
 // @namespace    https://github.com/Colin23/userscripts
-// @version      1.0.0
+// @version      1.1.0
 // @description  This user script changes the font color of files/folders in GitHub repositories to highlight recent changes.
 // @author       Colin Mörbe
 // @match        https://github.com/*/*
@@ -15,26 +15,45 @@
 (function () {
     "use strict";
 
-    // Helper function to check if a date is within the last month
-    function isRecent(dateString) {
-        const date = new Date(dateString);
+    /**
+     * Checks if the commit date is more recent than the age the method should check against.
+     *
+     * @param dateString The commit date
+     * @param ageInMonths The age in months that should be checked against
+     * @returns {boolean} Returns true if the commit date is more recent than the date to check against, returns false otherwise
+     */
+    function isRecent(dateString, ageInMonths) {
+        const commitDate = new Date(dateString);
         const now = new Date();
-        const oneMonthAgo = new Date();
-        oneMonthAgo.setMonth(now.getMonth() - 1);
-        return date > oneMonthAgo;
+        const ageToCalculateAgainst = new Date();
+        // This is safe to use.
+        // For example, passing 6 when in January will adjust the year and subtract the months starting from December
+        // (e.g., January 2024 – 6 months result in July 2023).
+        ageToCalculateAgainst.setMonth(now.getMonth() - ageInMonths);
+        return commitDate > ageToCalculateAgainst;
     }
 
-    // Function to update the directory commit age elements
+    /**
+     * Extracts the commit date, passes is into the {@link isRecent} method
+     * and changes the font color of the commit date depending on the return of the {@link isRecent} method.
+     */
     function highlightRecentChanges() {
-        // Select all directory commit age elements
+        // Select all commit age elements → This works for directories and files.
         const ageElements = document.querySelectorAll(".react-directory-commit-age relative-time");
 
         ageElements.forEach(element => {
-            const datetime = element.getAttribute("datetime"); // Get the datetime attribute
-            if (datetime && isRecent(datetime)) {
+            const datetime = element.getAttribute("datetime");
+            if (!datetime) {
+                return;
+            }
+            if (isRecent(datetime, 1)) {
                 // If the change is younger than one month, make it red
+                element.style.color = "green";
+                element.style.fontWeight = "bold";
+            } else if (isRecent(datetime, 3)) {
+                element.style.color = "yellow";
+            } else if (isRecent(datetime, 6)) {
                 element.style.color = "red";
-                element.style.fontWeight = "bold"; // Optional: make it bold for emphasis
             }
         });
     }
